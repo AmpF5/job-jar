f43e63bce2bb03ce8378fe56a79ad59f08aeda6c
Mateusz Kurzyna <lolc44t@gmail.com>
2025-08-20 17:29:50 +0200

    init

---
+++ JobJarApi/src/test/java/org/jobjar/jobjarapi/JobJarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jobjar.jobjarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobJarApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
