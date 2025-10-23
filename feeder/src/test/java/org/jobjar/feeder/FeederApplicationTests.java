ffc20139dff81f1449acfec60f8bf4e87c651374
Mateusz Kurzyna <lolc44t@gmail.com>
2025-10-23 18:42:50 +0200

    Init feeder

---
+++ feeder/src/test/java/org/jobjar/feeder/FeederApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jobjar.feeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeederApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
