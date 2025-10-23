d48774ec0cd306eb668ebf00a37f953dcb4574ad
Mateusz Kurzyna <lolc44t@gmail.com>
2025-10-23 18:30:47 +0200

    Add java muncher

---
+++ muncher/src/main/java/org/jobjar/muncher/MuncherApplication.java
@@ -0,0 +1,13 @@
+package org.jobjar.muncher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuncherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuncherApplication.class, args);
+    }
+
+}
---
+++ muncher/src/test/java/org/jobjar/muncher/MuncherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jobjar.muncher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuncherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
